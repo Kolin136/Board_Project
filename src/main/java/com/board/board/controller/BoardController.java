953169f667bd3348e4dfc5abc1aed44cd7ea5525
Kolin136 <jsowb136@gmail.com>
2023-11-06 14:43:32 +0900

    컨트롤러 변경 and 서비스 구현 완료

--- src/main/java/com/board/board/controller/BoardController.java
+++ src/main/java/com/board/board/controller/BoardController.java
@@ -35,13 +35,13 @@ public class BoardController {
     // 패스워드는 RequestHeader로 받을거라 RequestBody에(required = false)옵션을 줬지만 모든 필드에 다 적용인듯한...
     // 해결 방법은 특정 필드 required = false 적용이나 수정용 비밀번호 필드없는 RequestDto 새로 만들기??
     @PutMapping("/board/{id}")
-    public BoardResponseDto updateBoard(@PathVariable Long id, @RequestHeader("password") String password, @RequestBody(required = false) BoardRequestDto requestDto){
-        return boardService.updateBoard(id,password,requestDto);
+    public BoardResponseDto updateBoard(@PathVariable Long id, @RequestBody BoardRequestDto requestDto){
+        return boardService.updateBoard(id,requestDto);
     }
 
     @DeleteMapping("/board/{id}")
-    public Long deleteBoard(@PathVariable Long id,@RequestHeader("password") String password){
-        return boardService.deleteBoard(id,password);
+    public Long deleteBoard(@PathVariable Long id,@RequestBody(required = false) BoardRequestDto requestDto){
+        return boardService.deleteBoard(id,requestDto);
     }
 
 
--- src/main/java/com/board/board/service/BoardService.java
+++ src/main/java/com/board/board/service/BoardService.java
@@ -2,11 +2,14 @@ package com.board.board.service;
 
 import com.board.board.dto.BoardRequestDto;
 import com.board.board.dto.BoardResponseDto;
+import com.board.board.entity.Board;
 import com.board.board.repository.BoardRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
+@Transactional
 @Service
 public class BoardService {
 
@@ -17,18 +20,74 @@ public class BoardService {
     }
 
     public BoardResponseDto createBoard(BoardRequestDto requestDto) {
+        Board board = new Board(requestDto);
+
+        Board saveBoard = boardRepository.save(board);
+
+        BoardResponseDto boardResponseDto = new BoardResponseDto(saveBoard);
+
+//        BoardResponseDto boardResponseDto = new BoardResponseDto(boardRepository.save(board));
+
+        return boardResponseDto;
     }
 
+    @Transactional(readOnly = true)
     public BoardResponseDto getIdBoard(Long id) {
+
+        Board board = findBoard(id);
+
+        BoardResponseDto boardResponseDto = new BoardResponseDto(board);
+
+        return boardResponseDto;
     }
 
+    @Transactional(readOnly = true)
     public List<BoardResponseDto> getBoards() {
+
+        return boardRepository.findAll().stream().map(BoardResponseDto::new).toList();
+
     }
 
 
-    public BoardResponseDto updateBoard(Long id, String password, BoardRequestDto requestDto) {
+    public BoardResponseDto updateBoard(Long id, BoardRequestDto requestDto) {
+
+        Board board = findBoard(id);
+        BoardResponseDto boardResponseDto = null;
+
+        if (board.getPassword().equals(requestDto.getPassword())) {
+            board.update(requestDto);
+            boardResponseDto = new BoardResponseDto(board);
+        } else {
+            System.out.println("비번 틀린 메세지");
+        }
+
+        return boardResponseDto;
     }
 
-    public Long deleteBoard(Long id, String password) {
+    public Long deleteBoard(Long id, BoardRequestDto requestDto) {
+
+        Board board = findBoard(id);
+
+        if (board.getPassword().equals(requestDto.getPassword())) {
+            boardRepository.delete(board);
+
+        } else {
+            System.out.println("비번 틀린 메세지");
+        }
+
+        return id;
     }
+
+
+    //JpaRepository의 findById는 Optional리턴이라  null체크를 해줘야한다 그래서 orElseThrow 사용해서
+    //찾는값이 있으면 Board반환 하고 없으면 null이 반환되서 예외처리한다.
+    private Board findBoard(Long id) {
+        return boardRepository.findById(id).orElseThrow(() ->
+                new IllegalArgumentException("선택한 글은 존재하지 않습니다.")
+        );
+    }
+
+
+
+
 }
