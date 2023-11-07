3f7557c0ae1245b220cf43273dd5a56ed317f08d
Kolin136 <jsowb136@gmail.com>
2023-11-07 10:05:54 +0900

    컨트로러 deleteBoard 주석 추가

--- src/main/java/com/board/board/controller/BoardController.java
+++ src/main/java/com/board/board/controller/BoardController.java
@@ -66,7 +66,9 @@ public class BoardController {
 
     }
 
-
+    //슬랙의 다른분은 Delete할시 @RequestHeader를 이용한다 해서 조금 알아보니, 아직 정확한 이유나 원리는 잘 모르지만
+    //토큰,세션,jwt등 비밀번호 암호화를 하고 RequestHeader에 담기위해서 사용한다는듯하니
+    //지금은 토큰,세션,jwt등 아니라서 RequestBody에 하는게 맞죠? (그래서 비번만 받아도 가능하니 required = false 했습니다)
     @DeleteMapping("/board/{id}")
     public ResponseEntity<Object> deleteBoard(@PathVariable Long id,@RequestBody(required = false) BoardRequestDto requestDto){
         Long deleteId = boardService.deleteBoard(id,requestDto);
